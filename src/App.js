cb35d5834d2cb4ac2fdf8e189b9a756dd82691be
Miguel <miguezaga2@hotmail.com>
2022-09-19 09:30:30 -0500

    setup npm

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>New App</h1>
+      Victor Crest 26 London 80K Followers 803K Likes 1.4K Photos
+    </div>
+  );
+}
+
+export default App;
