21a36628305ebc842223f1752cdd4f4118e9b046
Miguel <miguezaga2@hotmail.com>
2022-09-19 10:13:18 -0500

    add html structure and classes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div>
-      <h1>New App</h1>
-      Victor Crest 26 London 80K Followers 803K Likes 1.4K Photos
+    <div className="wrapper">
+      <img className="bg-top" alt="" src="" />
+      <img className="bg-bottom" alt="" src="" />
+      <main className="card">
+        <img className="card__img" alt="" src="" />
+        <div className="card__container">
+          <div className="card__info">
+            <h1 className="card__name">Victor Crest</h1>
+            <h2 className="card__age">26</h2>
+            <h2 className="card__location">London</h2>
+          </div>
+          <ul className="card__stats">
+            <li className="card__stats-li">
+              <h3 className="card__stats-num">80K</h3>
+              <p className="card__stats-txt">Followers</p>
+            </li>
+            <li className="card__stats-li">
+              <h3 className="card__stats-num">803K</h3>
+              <p className="card__stats-txt">Likes</p>
+            </li>
+            <li className="card__stats-li">
+              <h3 className="card__stats-num">1.4K</h3>
+              <p className="card__stats-txt">Photos</p>
+            </li>
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
